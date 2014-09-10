b00b5779d7ef118dbb623ae2e2000bebe0efc343
Ben Cates <ben.cates@gmail.com>
2014-09-10 17:32:14 -0600

    Imported HTML from Jekyll site

---
+++ wp-content/themes/ideum/front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying the front page.
+ *
+ * This is the template that displays the front page for the
+ * site.
+ *
+ * Methods for TimberHelper can be found in the /functions sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+Timber::render(array('front-page.twig'), $context);
