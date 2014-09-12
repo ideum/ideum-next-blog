b35c6ac3351b634747318adf7fee5e1d6635fe68
glassk <glass@ideum.com>
2014-09-12 10:08:54 -0600

    replace static menus w/ dynamic wp + timber menus

--- wp-content/themes/ideum/functions.php
+++ wp-content/themes/ideum/functions.php
@@ -91,7 +91,9 @@
 			$context['foo'] = 'bar';
 			$context['stuff'] = 'I am a value set in your functions.php file';
 			$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-			$context['menu'] = new TimberMenu();
+			$context['tabs'] = new TimberMenu('pre-nav');
+			$context['nav'] = new TimberMenu('primary-nav');
+			$context['foot'] = new TimberMenu('footer-nav');
 			$context['site'] = $this;
 			return $context;
 		}
