b531506d2ac7e5cfd41d2833f3065159e6a9e0e7
glassk <glass@ideum.com>
2014-10-01 08:45:56 -0600

    modify single.php to follow/support design

--- wp-content/themes/ideum/single.php
+++ wp-content/themes/ideum/single.php
@@ -10,21 +10,28 @@
  */
 
 $context = Timber::get_context();
+$post = new TimberPost();
+
 $post = Timber::query_post();
 
 $post_cat = $post->get_terms('category');
 $post_cat = $post_cat[0]->ID;
 
 $context['post'] = $post;
+
 $context['wp_title'] .= ' - ' . $post->title();
+$context['wp_author'] .= ' - ' . $post->author();
+
 
 $context['acf'] = get_field_objects($data['post']->ID); 
+
 //$context['comment_form'] = TimberHelper::get_comment_form();
 
 $sidebar_context = array();
 $sidebar_context['related'] = Timber::get_posts('cat='.$post_cat.'&posts_per_page=3');
+
 $context['bottombar'] = Timber::get_sidebar('bottombar-related.twig', $sidebar_context);
-$context['sidebar'] = Timber::get_sidebar('sidebar-post-meta.twig', $sidebar_context);
+$context['sidebar'] = Timber::get_sidebar('sidebar.twig', $sidebar_context);
 
 if (post_password_required($post->ID)){
 	Timber::render('single-password.twig', $context);
