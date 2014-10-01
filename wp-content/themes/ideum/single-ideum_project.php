7dc39ae6e94ac35333322cddc354b20ae1c499cf
glassk <glass@ideum.com>
2014-10-01 08:45:03 -0600

    + tease, archive & single ideum-project php & twig

---
+++ wp-content/themes/ideum/single-ideum_project.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * Methods for TimberHelper can be found in the /functions sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+
+$context['title'] = $post->title();
+
+$context['acf'] = get_field_objects($data['post']->ID); 
+
+$sidebar_context = array();
+$sidebar_context['related'] = Timber::get_posts('post_type=ideum_project&cat='.$post_cat.'&posts_per_page=3');
+
+$context['bottombar'] = Timber::get_sidebar('bottombar-related-projects.twig', $sidebar_context);
+$context['sidebar'] = Timber::get_sidebar('sidebar.twig', $sidebar_context);
+
+Timber::render(array('single-ideum_project.twig'),  $context);
