454484893829195573830f2c56680651d4f959c2
glassk <glass@ideum.com>
2014-10-07 11:54:44 -0600

    remove space in index.php

--- wp-content/themes/ideum/index.php
+++ wp-content/themes/ideum/index.php
@@ -20,8 +20,6 @@
 	$context = Timber::get_context();
 	$context['posts'] = Timber::get_posts();
 
-
-
 	$templates = array('index.twig');
 	if (is_home()){
 		array_unshift($templates, 'home.twig');
