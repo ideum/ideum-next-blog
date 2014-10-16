8933f5ec071443d426ea7afa4d09ccd66461c871
glassk <glass@ideum.com>
2014-10-16 12:13:51 -0600

    adj colorbox dialog boxes behaviors

--- wp-content/themes/ideum/javascripts/colorbox.js
+++ wp-content/themes/ideum/javascripts/colorbox.js
@@ -16,15 +16,16 @@ $(document).ready(function(){
   $(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
   $(".vimeo").colorbox({iframe:true, innerWidth:500, innerHeight:409});
   $(".iframe").colorbox({iframe:true, width:"80%", height:"80%"});
-  $(".inline").colorbox({inline:true, width:"50%"});
-  $(".inline-share").colorbox({inline:true, width:"550px"});
+  $(".inline").colorbox({inline:true, width:"60%", height:"auto"});
+  $(".inline-share").colorbox({inline:true, width:"60%", height:"auto"});
   $(".member-dialog").colorbox({inline:true, width:"60%", height:"auto"});
+  $(".share-dialog").colorbox({inline:true, width:"60%", height:"auto"});
   $(".callbacks").colorbox({
     onOpen:function(){ alert('onOpen: colorbox is about to open'); },
     onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
     onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
     onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
-    onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
+    onClosed:function(){ alert('onClosed: colorbox has completely closed'); } 
   });
 
   $('.non-retina').colorbox({rel:'group5', transition:'none'})
