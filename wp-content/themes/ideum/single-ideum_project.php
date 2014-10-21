8b051fed12d9267c9616ab7f9e36b1e77aad1ea9
glassk <glass@ideum.com>
2014-10-21 13:41:19 -0600

    chng sidebar_context from array to timber get context

--- wp-content/themes/ideum/single-ideum_project.php
+++ wp-content/themes/ideum/single-ideum_project.php
@@ -17,9 +17,9 @@ $context['title'] = $post->title();
 $args = array('fields'=>'ids');
 $post_cats = $post->get_terms('category', $args);
 
-$context['acf'] = get_field_objects($data['post']->ID); 
+$context['acf'] = get_field_objects($data['post']->ID);
 
-$sidebar_context = array();
+$sidebar_context = Timber::get_context();
 $sidebar_context['related_projects'] = Timber::get_posts('post_type=ideum_project&cat='.$post_cats.'&posts_per_page=3'); //FIXME - don't know if this works
 $sidebar_context['thisproject'] = Timber::get_post($post->ID);
 
