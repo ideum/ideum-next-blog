7a229fd04a031a11134e318f81eeff7ac71acf1f
Ben Cates <ben@ideum.com>
2014-11-10 16:15:54 -0700

    Adds wiring for social media filter

--- wp-content/themes/ideum/javascripts/select-filters.js
+++ wp-content/themes/ideum/javascripts/select-filters.js
@@ -1,3 +1,14 @@
 'use strict';
 
 var App = angular.module('ideum.selectFilters', []);
+
+App.controller('socialFilterCtrl', [
+  '$scope', '$window',
+  function ($scope, $window) {
+    $scope.$watch('socialSource', function (source, oldSource) {
+      if (source === oldSource) return;
+
+      $window.location.search = 'source=' + source;
+    });
+  }
+]);
