f07fa1fb6b407bc1e6aaa700810d211ee298b2aa
Ben Cates <ben@ideum.com>
2014-11-11 16:42:59 -0700

    Fix for dropdown filters

--- wp-content/themes/ideum/javascripts/select-filters.js
+++ wp-content/themes/ideum/javascripts/select-filters.js
@@ -2,6 +2,17 @@
 
 var App = angular.module('ideum.selectFilters', []);
 
+App.controller('projectFilterCtrl', [
+  '$scope', '$window',
+  function ($scope, $window) {
+    $scope.$watch('projectType', function (type, oldType) {
+      if (type === oldType) return;
+
+      // FIXME: real filter
+    });
+  }
+]);
+
 App.controller('socialFilterCtrl', [
   '$scope', '$window',
   function ($scope, $window) {
