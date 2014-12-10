571208666370c2d4dc4bce62a2a2f00be9d2913c
glassk <glass@ideum.com>
2014-12-10 06:26:51 -0700

    + Products & Projects to Site Map

--- wp-content/themes/ideum/page.php
+++ wp-content/themes/ideum/page.php
@@ -51,6 +51,7 @@ if (is_page('team')){
 if (is_page('site-map')){ 
   $context['map_categories'] = Timber::get_terms('category');
   $context['map_posts'] = Timber::get_posts('post_type=post&post_status=publish&order=desc&posts_per_page=-1'); 
+  $context['map_projects'] = Timber::get_posts('post_type=ideum_project&post_status=publish&order=desc&posts_per_page=-1');
 }
 
 if (is_page('creative-services')) { // this is just-in-case someone lands here but the pagination does not work 
