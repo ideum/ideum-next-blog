bfe3a23aba6fc0136d3c39d33cef238b5fe09e2d
Glass <glass@ideum.com>
2015-03-16 11:27:55 -0600

    make related projects really related projects

--- wp-content/themes/ideum/single-ideum_project.php
+++ wp-content/themes/ideum/single-ideum_project.php
@@ -17,13 +17,19 @@ $context['title'] = $post->title();
 $context['post'] = $post;
 
 $args = array('fields'=>'ids');
-$post_cats = $post->get_terms('category', $args);
+// very ugly way to get a comma-delimited list of category ids for related posts
+$post_cats = $post->get_terms('category', $args); 
+if ($post_cats[0]->ID) { $post_cats_ids = $post_cats[0]->ID; } 
+if ($post_cats[1]->ID) { $post_cats_ids = $post_cats_ids.','.$post_cats[1]->ID; } 
+if ($post_cats[2]->ID) { $post_cats_ids = $post_cats_ids.','.$post_cats[2]->ID; } 
+if (!$post_cats_ids) { $post_cats_ids = '0'; } else { $post_cats_ids = $post_cats_ids.',15'; }
+//echo $post_cats_ids;
 
 $context['acf'] = get_field_objects($data['post']->ID);
 
 $sidebar_context = Timber::get_context();
 $sidebar_context['site_url'] = get_bloginfo('url');
-$sidebar_context['related_projects'] = Timber::get_posts('post_type=ideum_project&cat='.$post_cats.'&posts_per_page=3'); //FIXME - don't know if this works
+$sidebar_context['related_projects'] = Timber::get_posts('post_type=ideum_project&cat='.$post_cats_ids.'&posts_per_page=3'); //FIXME - don't know if this works
 $sidebar_context['thisproject'] = Timber::get_post($post->ID);
 
 $context['related_projects'] = Timber::get_sidebar('bar-related-projects.twig', $sidebar_context);
