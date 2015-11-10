2d91c8f839bdab67389e966248516838926cd300
Glass <glass@ideum.com>
2015-11-10 08:40:50 -0700

    add styles to contact success page to make more visible

--- wp-content/plugins/ideum-inquiries/templates/success.php
+++ wp-content/plugins/ideum-inquiries/templates/success.php
@@ -1,3 +1,4 @@
-<h3 style="margin-top:20%;" class="inquiryThanks">Thank You!</h3>
-
-<p class="inquiryThanks">We will be in touch with you shortly.</p>
+<div style="margin:20% auto 0;text-align:center;">
+	<h3 class="inquiryThanks">Thank You!</h3>
+	<p class="inquiryThanks">We will be in touch with you shortly.</p>
+</div>
